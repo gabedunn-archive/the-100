91eacbacbc49216db24c7c86b277ea822b3b0992
Gabe Dunn <gabe.dunn@shaw.ca>
2019-11-23 14:55:39 -0700

    update some stuff

--- server/index.js
+++ server/index.js
@@ -2,27 +2,27 @@ const Koa = require('koa')
 const Router = require('koa-router')
 const koaJson = require('koa-json')
 const bodyParser = require('koa-bodyparser')
-// const koaStatic = require('koa-static')
+const koaStatic = require('koa-static')
 const { config } = require('dotenv')
 const { resolve } = require('path')
 
-const { exchangeToken, revokeToken } = require('./auth')
 const { cors, responseTime, home } = require('./funcs')
 
 config({ path: resolve(process.cwd(), '.env') })
+
+const { join } = require('path')
 
 const app = new Koa()
 const r = new Router()
 
 app.use(koaJson())
 // app.use(koaStatic(join(__dirname, '..', 'dist')))
+app.use(koaStatic(join('/home', 'redxtech', 'media', 'tv', 'The 100', 'The 100 S01')))
 app.use(cors)
 app.use(responseTime)
 app.use(bodyParser())
 
 r.get('/', home)
-r.all('/exchange', exchangeToken)
-r.all('/revoke', revokeToken)
 
 app.use(r.routes())
 
--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,5 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/season/01">Season 01</router-link> |
-      <router-link to="/season/02">Season 02</router-link> |
-      <router-link to="/season/03">Season 03</router-link> |
-      <router-link to="/season/04">Season 04</router-link> |
-      <router-link to="/season/05">Season 05</router-link> |
-      <router-link to="/season/06">Season 06</router-link>
-    </div>
     <router-view/>
   </div>
 </template>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #00aab9;
-}
-</style>
